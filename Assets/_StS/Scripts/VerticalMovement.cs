f8102342b274b02cb51c76f4049416ed7e3c7a5f
Stefan <46993275+stefan17896@users.noreply.github.com>
2019-01-30 20:29:09 +0100

    Added Player object can move up and down by pressing arrow keys

---
+++ Assets/_StS/Scripts/VerticalMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
