614ffe143c1d60bc9db9a735dfba1fef87cc1670
stefan17896 <46993275+stefan17896@users.noreply.github.com>
2019-01-31 10:31:12 +0100

    Added start movement speed

--- Assets/_StS/Scripts/VerticalMovement.cs
+++ Assets/_StS/Scripts/VerticalMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class VerticalMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
