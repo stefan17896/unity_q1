7d98d6911edf57af363e3d3db1e8dcc49dbc3a6b
Stefan <46993275+stefan17896@users.noreply.github.com>
2019-01-31 14:30:38 +0100

    Player object can now be controlled by mouse and by pressing F2 can be controlled by the Arrow Keys

--- Assets/_StS/Scripts/VerticalMovement.cs
+++ Assets/_StS/Scripts/VerticalMovement.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class VerticalMovement : MonoBehaviour
 {
-    public float moveSpeed;
+    public float moveSpeed = 10f;
+    public float verticalSpeed = 0.5f;
+    public float horizontalSpeed = 0.5f;
+    bool mode = false;
+
+    public bool Mode { get => mode; set => mode = value; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,14 +20,34 @@ public class VerticalMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Mode)
         {
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            }
         }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if(!Mode){
+            float h = Input.GetAxis("Mouse Y") * verticalSpeed;
+            float v = Input.GetAxis("Mouse X") * horizontalSpeed;
+            float t = h + 30;
+            transform.Translate(v, h, 0);
+
         }
 
+        if (Input.GetKeyDown(KeyCode.F2)){ Mode = true; }
+        if (Input.GetKeyDown(KeyCode.F1)){ Mode = false; }
     }
 }
